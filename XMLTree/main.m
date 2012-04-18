dc4188f7a419ce9b235c827e81418f7eec6356d9
Saam Barati <saambarati1@gmail.com>
2012-04-18 14:56:01 -0700

    Initial Commit

---
+++ XMLTree/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XMLTree
+//
+//  Created by Saam Barati on 4/18/12.
+//  Copyright (c) 2012 Saam Barati Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
